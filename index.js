35bdfd770f7d2f801d9ce47bd3477f91673ab149
Michael Sliger <michaelj.sliger@gmail.com>
2020-08-11 09:53:58 -0600

    second commit, html and styles started

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+const STORE = [
+  {
+    id: 1,
+    question: 'When was Halo first released?',
+    answers: []
+  },
+  {
+    id: 2,
+    question: 'Who is the main character?',
+    answers: []
+  },
+  {
+    id: 3,
+    question: 'What game studio originally developed Halo?',
+    answers: []
+  },
+  {
+    id: 4,
+    question: 'Who is the main character?',
+    answers: []
+  },
+  {
+    id: 5,
+    question: 'Who is the main character?',
+    answers: []
+  }
+  
+];
