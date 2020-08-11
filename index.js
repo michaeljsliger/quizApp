50182aaca69c6a04aab839df71c9cb1e873eb477
Michael Sliger <michaelj.sliger@gmail.com>
2020-08-11 10:07:24 -0600

    initialized store for questions

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 'use strict';
 const STORE = [
   {
-    id: 1,
+      id: 1,
+    ];
+    // When was halo first released? 'November 15 2000': false, 'November 15, 2001': true, 'November 15, 20002': false, 'November 15, 2003': false
     question: 'When was Halo first released?',
-    answers: []
+    answers: [
+      ['November 15 2000', false], 
+      ['November 15, 2001', true],
+      ['November 15, 20002': false],
+      ['November 15, 2003', false] 
+    ]
   },
   {
     id: 2,
+    // what is the main characters name? 'Master Chief John-117': True, 'Cortana': false, 'Spartan': false, 'Barney': false
     question: 'Who is the main character?',
-    answers: []
+    answers: [
+        ['Master Chief John-117', true],
+        ['Cortana', false],
+        ['Spartan', false],
+        ['Barney', false]
+    ]
   },
   {
     id: 3,
+    // What game studio originally developed Halo? 'Bungie': true, '343 Industries': false, 'Creative Assembly': false, Ensemble Studios: false
     question: 'What game studio originally developed Halo?',
-    answers: []
+    answers: [
+        ['Bungie', true],
+        ['343 Industries', false],
+        ['Creative Assembly', false],
+        ['Ensemble Studios', false]
+    ]
   },
   {
     id: 4,
+    // How many games were developed by the Halo Franchise? 16: True, 5: false, 8: false, 9: false
     question: 'Who is the main character?',
-    answers: []
+    answers: [
+        [5, false],
+        [8, false],
+        [16, true],
+        [9, false]
+    ]
   },
   {
     id: 5,
+    // How much is the Halo franchise worth? '$5 billion': true, '$3 billion': false, '$6 billion': false, '$7 Billion': false
     question: 'Who is the main character?',
-    answers: []
+    answers: [
+        ['$3 Billion', false],
+        ['$6 Billion', false],
+        ['$7 Billion', false],
+        ['$5 Billion', true]
+        
+    ]
   }
   
-];
+
+
+
+
